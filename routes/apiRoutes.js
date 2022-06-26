956646ab19b8f5a2c2a6e52fcd02abf648a89160
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-06-26 17:04:05 +0100

    Changed "/api/notes" to "/public/notes"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,14 +3,15 @@ const fs = require("fs");
 const uniqid = require("uniqid");
 const router = express.Router();
 
-router.get("/api/notes", (req, res) => {
+router.get("/public/notes", (req, res) => {
   fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     return res.json(JSON.parse(data));
   });
 });
 
-router.post("/api/notes", (req, res) => {
+//Adds note
+router.post("/public/notes", (req, res) => {
   fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     const dbData = JSON.parse(data);
@@ -24,6 +25,25 @@ router.post("/api/notes", (req, res) => {
       if (err) throw err;
       return res.json(dbData);
     });
+  });
+});
+
+//Deletes note
+router.delete("/public/notes/:id", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const dbData = JSON.parse(data);
+    const noteId = req.params.id;
+
+    for (let i = 0; i < dbData.length; i++) {
+      if (noteId === dbData[i].id) {
+        dbData.splice([i], 1);
+        fs.writeFile("db/db.json", JSON.stringify(dbData), (err) => {
+          if (err) throw err;
+          return res.json(dbData);
+        });
+      }
+    }
   });
 });
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+//Check routes
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+//Check routes
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
